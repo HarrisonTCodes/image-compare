cdb0823e159be1375d94f7973d3a88f455e31e51
HarrisonTCodes <htillbackup@gmail.com>
2025-06-06 23:12:57 +0100

    refactor: cleaned up image parsing

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,19 @@ mod image_mutation;
 
 fn main() {
     let path1 = "../../../Downloads/test1.png";
-
-    let img1 = match image::open(path1) {
-        Ok(img) => img,
-        Err(e) => {
-            eprintln!("Error: {}", e);
-            std::process::exit(1);
-        }
-    };
-
-    let dhash1 = hash::dhash(&img1, true);
-
     let path2 = "../../../Downloads/test4.png";
 
-    let img2 = match image::open(path2) {
-        Ok(img) => img,
-        Err(e) => {
-            eprintln!("Error: {}", e);
-            std::process::exit(1);
-        }
-    };
+    let img1 = image::open(path1).unwrap_or_else(|e| {
+        eprintln!("Error opening image {}: {}", path1, e);
+        std::process::exit(1);
+    });
 
-    let dhash2 = hash::dhash(&img2, true);
+    let img2 = image::open(path2).unwrap_or_else(|e| {
+        eprintln!("Error opening image {}: {}", path1, e);
+        std::process::exit(1);
+    });
 
-    let dist = hash::hamming_distance(dhash1, dhash2);
+    let dist = hash::hamming_distance(hash::dhash(&img1, true), hash::dhash(&img2, true));
 
     println!("{}%", (1.0 - dist as f64 / 64.0) * 100.0);
 }
