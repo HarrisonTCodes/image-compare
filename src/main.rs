eef85e1137de96f6493fde7cf1baea071a5b2b50
HarrisonTCodes <htillbackup@gmail.com>
2025-06-06 22:14:02 +0100

    feat: added initial dhash function with image resizing and drawing

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use image::GenericImageView;
+use image::{DynamicImage, imageops::FilterType};
 
 fn main() {
     let path = "../../../Downloads/carrots1.webp";
@@ -11,8 +11,20 @@ fn main() {
         }
     };
 
-    let dimensions = img.dimensions();
-    let pixel = img.get_pixel(dimensions.0 / 2, dimensions.1 / 2);
+    dhash(&img)
+}
 
-    println!("{:?}", pixel);
+fn dhash(img: &DynamicImage) {
+    let resized = img.resize_exact(9, 8, FilterType::Lanczos3);
+    let rgb = resized.to_rgb8();
+
+    for y in 0..rgb.height() {
+        for x in 0..rgb.width() {
+            let pixel = rgb.get_pixel(x, y);
+            let [r, g, b] = pixel.0;
+
+            print!("\x1b[38;2;{r};{g};{b}m█");
+        }
+        println!("\x1b[0m");
+    }
 }
