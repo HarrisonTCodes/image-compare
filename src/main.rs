a7ef09418c0c31f036ce90b5caa7505d00e64244
HarrisonTCodes <htillbackup@gmail.com>
2025-06-06 22:21:16 +0100

    feat: added visualise flag to dhash function

--- src/main.rs
+++ src/main.rs
@@ -11,20 +11,25 @@ fn main() {
         }
     };
 
-    dhash(&img)
+    dhash(&img, true)
 }
 
-fn dhash(img: &DynamicImage) {
+fn dhash(img: &DynamicImage, visualise: bool) {
     let resized = img.resize_exact(9, 8, FilterType::Lanczos3);
-    let rgb = resized.to_rgb8();
 
-    for y in 0..rgb.height() {
-        for x in 0..rgb.width() {
-            let pixel = rgb.get_pixel(x, y);
-            let [r, g, b] = pixel.0;
+    if visualise {
+        let rgb = resized.to_rgb8();
+        println!("Resized image:");
+        for y in 0..rgb.height() {
+            for x in 0..rgb.width() {
+                let pixel = rgb.get_pixel(x, y);
+                let [r, g, b] = pixel.0;
 
-            print!("\x1b[38;2;{r};{g};{b}m█");
+                // Draw pixel with RGB colour
+                print!("\x1b[38;2;{r};{g};{b}m█");
+            }
+            // Reset colours for new line
+            println!("\x1b[0m");
         }
-        println!("\x1b[0m");
     }
 }
