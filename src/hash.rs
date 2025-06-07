cad7a0667750c478b73528a2cd630b4d243d1978
HarrisonTCodes <htillbackup@gmail.com>
2025-06-07 14:30:22 +0100

    feat: improved display of dhash fingerprint

--- src/hash.rs
+++ src/hash.rs
@@ -1,25 +1,14 @@
 use crate::visualise;
 use image::{DynamicImage, GenericImageView, imageops::FilterType};
+use itertools::izip;
 
 pub fn dhash(img: &DynamicImage, visualise: bool) -> u64 {
     // Mutate image
     let resized = img.resize_exact(9, 8, FilterType::Lanczos3);
     let gray = resized.grayscale();
-    if visualise {
-        let squashed_disp = visualise::pixel_display(&resized);
-        let gray_disp = visualise::pixel_display(&gray);
-
-        println!("Squash -> Grayscale");
-        for (squashed_disp, gray_disp) in squashed_disp.lines().zip(gray_disp.lines()) {
-            println!("{} | {}", squashed_disp, gray_disp);
-        }
-    }
 
     // Calculate hash
     let mut hash: u64 = 0;
-    if visualise {
-        println!("dHash fingerprint:")
-    }
     for y in 0..8 {
         for x in 0..8 {
             let left_pixel_intensity = gray.get_pixel(x, y)[0];
@@ -28,17 +17,22 @@ pub fn dhash(img: &DynamicImage, visualise: bool) -> u64 {
             hash <<= 1; // Shift hash bits left
             if left_pixel_intensity > right_pixel_intensity {
                 hash |= 1; // Make LSB 1
-
-                if visualise {
-                    print!("▉")
-                }
-            } else if visualise {
-                print!(" ")
             }
         }
+    }
 
-        if visualise {
-            println!()
+    if visualise {
+        let squashed_disp = visualise::pixel_display(&resized);
+        let gray_disp = visualise::pixel_display(&gray);
+        let fingerprint_disp = visualise::u64_fingerprint_display(&hash);
+
+        println!("Squash -> Grayscale -> dHash");
+        for (squashed_line, gray_line, fingerprint_line) in izip!(
+            squashed_disp.lines(),
+            gray_disp.lines(),
+            fingerprint_disp.lines()
+        ) {
+            println!("{} | {} | {}", squashed_line, gray_line, fingerprint_line);
         }
     }
 
