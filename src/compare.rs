db575353f7b3bdc6135a1fb653cc47759c134eca
HarrisonTCodes <htillbackup@gmail.com>
2025-06-07 14:40:26 +0100

    refactor: cleaned up compare function calculating hashes outside of distance calculation

--- src/compare.rs
+++ src/compare.rs
@@ -5,13 +5,15 @@ pub fn compare_images(path1: &str, path2: &str, visualise: bool) -> f64 {
         eprintln!("Error opening image {}: {}", path1, e);
         std::process::exit(1);
     });
-
     let img2 = image::open(path2).unwrap_or_else(|e| {
         eprintln!("Error opening image {}: {}", path2, e);
         std::process::exit(1);
     });
 
-    let dist = hash::hamming_distance(hash::dhash(&img1, visualise), hash::dhash(&img2, visualise));
+    let hash1 = hash::dhash(&img1, visualise);
+    let hash2 = hash::dhash(&img2, visualise);
+
+    let dist = hash::hamming_distance(hash1, hash2);
 
     (1.0 - dist as f64 / 64.0) * 100.0
 }
