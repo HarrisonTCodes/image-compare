159066de58fe9a25243d2a245474fdf92c81c805
HarrisonTCodes <htillbackup@gmail.com>
2025-06-08 11:55:59 +0100

    feat: improved compare message output and used raw distance instead of percentage

--- src/compare.rs
+++ src/compare.rs
@@ -2,6 +2,7 @@ use crate::hash;
 use colored::*;
 
 pub fn compare_images(path1: &str, path2: &str, visualise: bool) {
+    // Get images
     let img1 = image::open(path1).unwrap_or_else(|e| {
         eprintln!("Error opening image {}: {}", path1, e);
         std::process::exit(1);
@@ -11,25 +12,42 @@ pub fn compare_images(path1: &str, path2: &str, visualise: bool) {
         std::process::exit(1);
     });
 
+    // Compute and output hashes
     let hash1 = hash::dhash(&img1, visualise);
     let hash2 = hash::dhash(&img2, visualise);
+    println!("{} dHash fingerprint - {}", path1.bold(), hash1);
+    println!("{} dHash fingerprint - {}", path2.bold(), hash2);
 
+    // Compute and output hamming distance
     let dist = hash::hamming_distance(hash1, hash2);
-    let percentage = (1.0 - dist as f64 / 64.0) * 100.0;
+    let mut message = format!("Hamming distance {}/64 - ", dist);
 
-    let message = match percentage {
-        p if p == 100.0 => format!("{}% - Images are indistinguishable", p)
-            .green()
-            .bold(),
-        p if p >= 90.0 => format!("{}% - Images are very likely the same", p)
-            .green()
-            .bold(),
-        p if p >= 80.0 => format!("{}% - Images are likely the same", p).green(),
-        p if p >= 65.0 => format!("{}% - Images are possibly the same", p).yellow(),
-        p => format!("{}% - Images are likely not the same", p)
-            .red()
-            .bold(),
-    };
+    match dist {
+        0 => {
+            message.push_str("images are indistinguishable");
+            message = message.green().bold().to_string();
+        }
+
+        1..=5 => {
+            message.push_str("images are very likely the same");
+            message = message.green().bold().to_string();
+        }
+
+        6..=10 => {
+            message.push_str("images are likely the same");
+            message = message.green().to_string();
+        }
+
+        11..=15 => {
+            message.push_str("images possibly the same");
+            message = message.yellow().to_string();
+        }
+
+        _ => {
+            message.push_str("images are likely not the same");
+            message = message.red().bold().to_string();
+        }
+    }
 
     println!("{}", message)
 }
