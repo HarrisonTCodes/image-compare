050e3911b418ca37acb42d55481430bf7d006cd6
HarrisonTCodes <htillbackup@gmail.com>
2025-06-12 17:08:36 +0100

    feat: added ahash algorithm

--- src/hash.rs
+++ src/hash.rs
@@ -34,3 +34,42 @@ pub fn dhash(img: &DynamicImage, visualise: bool) -> u64 {
 
     hash
 }
+
+pub fn ahash(img: &DynamicImage, visualise: bool) -> u64 {
+    // Mutate image
+    let resized = img.resize_exact(8, 8, FilterType::Lanczos3);
+    let gray = resized.grayscale();
+
+    // Calculate average intensity
+    let mut intensities: Vec<u8> = vec![];
+    for y in 0..8 {
+        for x in 0..8 {
+            intensities.push(gray.get_pixel(x, y)[0]);
+        }
+    }
+    let avg_intensity =
+        intensities.iter().map(|&x| x as u32).sum::<u32>() / intensities.len() as u32;
+
+    // Calculate hash
+    let mut hash: u64 = 0;
+    for intensity in intensities {
+        hash <<= 1; // Shift hash bits left
+        if intensity as u32 > avg_intensity {
+            hash |= 1; // Make LSB 1
+        }
+    }
+
+    if visualise {
+        let squashed_disp = visualise::pixel_display(&resized);
+        let gray_disp = visualise::pixel_display(&gray);
+        let fingerprint_disp = visualise::u64_fingerprint_display(&hash);
+
+        println!("{:<21}->{:<23}->{:<23}", "Squash", "  Grayscale", "  aHash");
+        println!(
+            "{}",
+            visualise::join_displays(vec![squashed_disp, gray_disp, fingerprint_disp])
+        )
+    }
+
+    hash
+}
